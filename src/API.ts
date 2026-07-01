e0e82a9d289bf6c0fb846dfc9bc92f41b347345b
bogdanm <bogdanmihankov@icloud.com>
2026-07-01 10:22:34 +0400

    Third

--- src/API.ts
+++ src/API.ts
@@ -1,4 +1,15 @@
+import { shuffleArray } from "./utils";
 
+export type Question = {
+    category: string;
+    correct_answer: string;
+    difficulty: string;
+    incorrect_answer: string[];
+    question: string;
+    type: string;
+}
+
+export type QuestionString = Question & { answers: string[] };
 
 export enum Difficulty {
     EASY = "easy",
@@ -7,5 +18,12 @@ export enum Difficulty {
 }
 
 export const fetchQuizQuestions = async(amount: number, difficulty: Difficulty) => {
-    
+    const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
+    const data = await (await fetch(endpoint)).json();
+    return data.results.map((question: Question) => (
+        {
+            ...question,
+            answer: shuffleArray([...question.incorrect_answer, question.correct_answer])
+        }
+    ))
 }
